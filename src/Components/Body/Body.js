86504c89387fa064326cc2f44640b9b36569ace5
sartajdev007 <sartaj5akik@gmail.com>
2022-09-29 22:55:48 +0600

    details component add

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './Body.css'
 import logoImage from '../../images/bodybuilder.png'
-import userImg from '../../images/user.png'
 import Activity from '../Activity/Activity';
+import Details from '../Details/Details';
 
 const Body = () => {
     const [activities, setActivity] = useState([]);
@@ -33,46 +33,7 @@ const Body = () => {
                 </div>
             </div>
             <div className='details-container'>
-                <div className='user'>
-                    <img src={userImg} alt="" />
-                    <div>
-                        <h3>Sartaj Akik</h3>
-                        <p><small>California,US</small></p>
-                    </div>
-                </div>
-                <div className="user-details">
-                    <div>
-                        <h3>80kg</h3>
-                        <p>Weight</p>
-                    </div>
-                    <div>
-                        <h3>5.8</h3>
-                        <p>Height</p>
-                    </div>
-                    <div>
-                        <h3>22</h3>
-                        <p>Age</p>
-                    </div>
-                </div>
-                <div className='time'>
-                    <button className='minutes'>
-                        1m
-                    </button>
-                    <button className='minutes'>
-                        2m
-                    </button>
-                    <button className='minutes'>
-                        5m
-                    </button>
-                    <button className='minutes'>
-                        10m
-                    </button>
-                </div>
-                <div className='exercise-details'>
-                    <h2>Exercise Details</h2>
-                    <input type="text" placeholder='exercise-time' className='ex-time' />
-                    <input type="text" placeholder='break-time' className='br-time' />
-                </div>
+                <Details></Details>
             </div>
         </div >
     );
